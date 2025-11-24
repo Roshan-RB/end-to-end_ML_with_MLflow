7fbf554c4d6193f416e67a5f8714a4483c2e890e
Roshan-RB <roshanrb001@gmail.com>
2025-11-24 21:50:27 +0100

    model evaluation and MLflow added

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipe
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
+import dagshub
+dagshub.init(
+    repo_owner="Roshan-RB",
+    repo_name="end-to-end_ML_with_MLflow",
+    mlflow=True
+)
+
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -41,6 +50,17 @@ STAGE_NAME = "Model Training Stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    obj = ModelTrainerTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
+
+
+STAGE_NAME = "Data Evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = ModelEvaluationTrainingPipeline()
    obj.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/mlProject/components/model_evaluation.py
@@ -0,0 +1,60 @@
+import os
+import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import joblib
+from mlProject.entity.config_entity import ModelEvaluationConfig
+from pathlib import Path
+from mlProject.utils.common import save_json
+
+class ModelEvaluation:
+    def __init__(self, config: ModelEvaluationConfig):
+        self.config = config
+
+    
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+
+    def log_into_mlflow(self):
+
+        test_data = pd.read_csv(self.config.test_data_path)
+        model = joblib.load(self.config.model_path)
+
+        test_x = test_data.drop([self.config.target_column], axis=1)
+        test_y = test_data[[self.config.target_column]]
+
+        # TRACKING → DagsHub
+        mlflow.set_tracking_uri(self.config.mlflow_uri)
+
+        # REGISTRY → Must be local (DagsHub does NOT support registry)
+        mlflow.set_registry_uri("file:///tmp/mlruns")
+
+        with mlflow.start_run():
+
+            predicted = model.predict(test_x)
+            rmse, mae, r2 = self.eval_metrics(test_y, predicted)
+
+            save_json(Path(self.config.metric_file_name),
+                    {"rmse": rmse, "mae": mae, "r2": r2})
+
+            mlflow.log_params(self.config.all_params)
+
+            mlflow.log_metric("rmse", rmse)
+            mlflow.log_metric("mae", mae)
+            mlflow.log_metric("r2", r2)
+
+            # Save model using joblib (NO registry involved)
+            local_model_path = "artifacts/model_evaluation/model.joblib"
+            joblib.dump(model, local_model_path)
+
+            # Log the joblib model file as a regular artifact
+            mlflow.log_artifact(local_model_path)
+
+
+        print("Logged successfully to DagsHub MLflow!")
